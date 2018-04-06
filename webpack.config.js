798162d07f39d2f88d67bb6e20237a85680d3501
Nwoke Promise <promisenwoke@gmail.com>
2018-04-06 02:53:23 +0100

    UI

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,8 @@ if (process.env.NODE_ENV === 'test') {
     require('dotenv').config({ path: '.env.test' });
 } else if (process.env.NODE_ENV === 'development') {
     require('dotenv').config({ path: '.env.development' });
+} else if (process.env.NODE_ENV === 'production') {
+    require('dotenv').config({ path: '.env.development' });
 }
 
 module.exports = (env) => {
